41912a35e33c1cfa2b1ea1c17603c7cdfd4b1b85
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-10-28 13:11:58 +0530

    added required dependencies

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+
+    This project will help the users to jot down any important lists or stuff they intend to do in a particular day.
+    This is emphasised on the simplicity in terms of the elegant user interface and experience.
+    We aim into bringing useful interactive applications, along with the ease of access being the center of focus.
+    This is the main server side code of the TODO application.
+
+*/
+
+// Author: Tulsi Prasad
+// Start Date: 28 October 2019
+// Project Name: TODO-APP
+
+    
