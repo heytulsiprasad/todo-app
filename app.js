a0afd1f6e830d8210d6dac2513ff1204a67160d6
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-10-28 16:48:13 +0530

    0 evening 16:47

--- app.js
+++ app.js
@@ -11,4 +11,48 @@
 // Start Date: 28 October 2019
 // Project Name: TODO-APP
 
-    
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+let items = [];
+
+app.get("/", function (req, res) {
+    // res.send("Hello");
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        items: items
+    });
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    // console.log(item);
+
+    items.push(item);
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
